514da8bbf254c53dbd75998166759e9d3dd843e1
herokutest <herokutest@MacBook-Pro-de-Arol.local>
2012-05-02 09:36:35 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var restify = require('restify');
+
+var server = restify.createServer();
+
+server.get('/hello/:name', function(req, res, next){
+	res.send(200,"Hola mon")
+	
+	return next()
+});
+
+server.get('/', function(req, res, next){
+	res.send(200, "Hola mon")
+	
+	return next()
+})
+
+//GET /hello/:nom/:edat
+server.get(/hello\/([\D]+)\/([\d]+)/,function(req,res,next){
+	// res.send(200,"Hola "+req.params[0]+" amb edat "+req.params[1])
+	var attributs = new Array("alt", "ric", "guapo")
+	
+	var json = {
+		nom: req.params[0],
+		edat: req.params[1],
+		atributs: attributs
+	}
+	res.send(200, json)
+	
+	return next()
+})
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
